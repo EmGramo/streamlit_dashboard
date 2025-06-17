182a86a785091849a8eb275003d37473ead7dea0
EmGramo <egrajalesm@gmail.com>
2025-06-17 17:49:52 -0500

    Update app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+st.header('Análisis de Vehículos en Venta - USA')
+car_data = pd.read_csv('vehicles_us.csv')
+
+# Botón para histograma
+if st.button('Construir histograma'):
+    st.write('Histograma de odómetro')
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+# Botón para scatter plot
+if st.button('Construir diagrama de dispersión'):
+    st.write('Diagrama de odómetro vs precio')
+    fig = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig, use_container_width=True)
